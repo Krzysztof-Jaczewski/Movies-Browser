7a2364a170baef9193ecf58a47d865d04e1dd25a
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-08 07:18:54 +0200

    added globalsyles and ThemeProvider

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+  margin: 0;
+}
+
+button {
+  cursor: pointer;
+}
+
+`;
---
+++ src/theme.js
@@ -0,0 +1,3 @@
+export const theme = {
+  colors: { Mercury: "#E5E5E5" },
+};
