c4901d0fcbe14586f725fc9649698d55b0173c7a
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-08 16:26:30 +0200

    created common container

---
+++ src/common/Container/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-gap: 24px;
+  max-width: 1368px;
+  height: 1391px;
+  margin: 0 auto;
+`;
