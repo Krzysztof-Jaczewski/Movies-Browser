a0d309682f3d688a50d322bcb130eb154587dec1
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-09-14 14:42:31 +0200

    PageList dodany do App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Navigation } from "./common/Navigation";
+import { PageList } from "./common/PageList";
 import { MovieList } from "./features/MovieList";
 
 export const App = () => (
   <>
     <Navigation />
     <MovieList />
+    <PageList />
   </>
 );
