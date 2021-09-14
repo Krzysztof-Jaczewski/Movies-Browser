f4a6d0bc1618c125de5d1e2183bceb24b448d230
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-14 12:00:57 +0200

    fertched genres

--- src/features/fetchGenres.js
+++ src/features/fetchGenres.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { API_Key, baseSiteUrl, language } from "./ApiParameters";
 
 export const useGenres = () => {
   const [genre, setGenre] = useState();
 
-  const API_Key = "f7171889d3718d0e9b300e81d35e65ae";
   useEffect(() => {
     const getGenres = async () => {
       try {
         const response = await axios.get(
-          `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_Key}`
+          `${baseSiteUrl}genre/movie/list?api_key=${API_Key}&language=${language}`
         );
         const result = await response.data.genres;
         console.log(result);
