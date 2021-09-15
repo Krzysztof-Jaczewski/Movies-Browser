e811f2db9ee102c011b957d9ca5749256c057ca2
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-09-15 06:35:18 +0200

    props in Button deleted

--- src/common/PageList/index.js
+++ src/common/PageList/index.js
@@ -4,7 +4,7 @@ import { Button, ButtonText, ButtonWrapper, Pages, PagesWrapper } from "./styled
 
 export const PageList = () => (
     <ButtonWrapper>
-        <Button first><ArrowLeft /><ButtonText>First</ButtonText></Button>
+        <Button><ArrowLeft /><ButtonText>First</ButtonText></Button>
         <Button><ArrowLeft /><ButtonText>Previous</ButtonText></Button>
             <PagesWrapper>
                 <Pages>Page</Pages>
@@ -12,7 +12,7 @@ export const PageList = () => (
                 <Pages>of</Pages>
                 <Pages number>500</Pages>
             </PagesWrapper>
-        <Button right><ButtonText>Next</ButtonText><ArrowRight /></Button>
-        <Button right last><ButtonText>Last</ButtonText><ArrowRight /></Button>
+        <Button ><ButtonText>Next</ButtonText><ArrowRight /></Button>
+        <Button><ButtonText>Last</ButtonText><ArrowRight /></Button>
     </ButtonWrapper>
 )
