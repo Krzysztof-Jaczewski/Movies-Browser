ac0bc3f844f8b0c225d9b262670d64203d9b5157
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-19 11:28:57 +0200

    Adde person prop to Container

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,5 +1,5 @@
 import { StyledContainer } from "./styled";
 
-export const Container = ({ children }) => {
-  return <StyledContainer>{children}</StyledContainer>;
+export const Container = ({ children, person }) => {
+  return <StyledContainer person={person}>{children}</StyledContainer>;
 };
