6141aad99301f20bff73fb46e2e48981bf5a8578
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-19 11:38:40 +0200

    added PeopleList to App

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,36 +1,42 @@
+import { useDispatch } from "react-redux";
 import { HashRouter } from "react-router-dom";
+import { fetchMovies } from "../../features/MoviesList/moviesSlice";
 import {
-    Link,
-    LinkItem,
-    Links,
-    LogoHeader,
-    LogoWrapper,
-    StyledInput,
-    StyledLogo,
-    StyledNavigation,
-    Wrapper
+  Link,
+  LinkItem,
+  Links,
+  LogoHeader,
+  LogoWrapper,
+  StyledInput,
+  StyledLogo,
+  StyledNavigation,
+  Wrapper,
 } from "./styled";
 
-export const Navigation = () => (
+export const Navigation = () => {
+  const dispatch = useDispatch();
+
+  return (
     <StyledNavigation>
-        <Wrapper>
-            <LogoWrapper>
-                <StyledLogo />
-                <LogoHeader>
-                    Movies Browser
-                </LogoHeader>
-            </LogoWrapper>
-            <Links>
-                <HashRouter>
-                    <LinkItem>
-                        <Link to="/">Movies</Link>
-                    </LinkItem>
-                    <LinkItem>
-                        <Link to="/">People</Link>
-                    </LinkItem>
-                </HashRouter>
-            </Links>
-        </Wrapper>
-        <StyledInput placeholder="Search for movies..." />
-    </StyledNavigation >
-);
+      <Wrapper>
+        <LogoWrapper>
+          <StyledLogo />
+          <LogoHeader>Movies Browser</LogoHeader>
+        </LogoWrapper>
+        <Links>
+          <HashRouter>
+            <LinkItem>
+              <Link to="/" onClick={() => dispatch(fetchMovies())}>
+                Movies
+              </Link>
+            </LinkItem>
+            <LinkItem>
+              <Link to="/">People</Link>
+            </LinkItem>
+          </HashRouter>
+        </Links>
+      </Wrapper>
+      <StyledInput placeholder="Search for movies..." />
+    </StyledNavigation>
+  );
+};
