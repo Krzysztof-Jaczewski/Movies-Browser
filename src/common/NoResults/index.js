5bd6ad321ae4ac1476aa13266270d2bea6beb7e8
pawlo4-94 <pawlo4-94@o2.pl>
2021-09-20 21:35:44 +0200

    NoResult image updated

--- src/common/NoResults/index.js
+++ src/common/NoResults/index.js
@@ -1,10 +1,9 @@
 import { Image } from "./styled";
 import { Header } from "../../common/Header";
-import NoResultImage from "../../images/picture.jpg";
 
 export const NoResult = () => (
     <>
         <Header title={`Sorry, there are no results for "Muan"`} />
-        <Image src={NoResultImage} />
+        <Image />
     </>
 );
--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
+import { ReactComponent as NoResultImage } from "../../images/picture.svg";
 
-export const Image = styled.img`
+export const Image = styled(NoResultImage)`
     display: block;
-    margin: 40px auto 0;
+    margin: 40px auto;
     width: 668px;
+    height: 508.63px;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax})
     {
         width: 222px;
+        height: 169.54px;
     }
 `;
