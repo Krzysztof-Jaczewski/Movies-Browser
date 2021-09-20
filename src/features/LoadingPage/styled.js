16ae063cdacb4bdcce81f5a79a6fb1cad1526211
pawlo4-94 <pawlo4-94@o2.pl>
2021-09-20 17:44:59 +0200

    margin-top for loading image added

--- src/features/LoadingPage/styled.js
+++ src/features/LoadingPage/styled.js
@@ -3,12 +3,13 @@ import { ReactComponent as Spinner } from "../../images/icon-spinner.svg";
 
 export const LoadingImage = styled(Spinner)`
     display: block;
-    margin: 0 auto;
+    margin: 120px auto 0;
     width: 91px;
     height: 91px;
     animation: spin 1s linear infinite;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+        margin: 24px auto 0;
         width: 35px;
         height: 35px;
     };
