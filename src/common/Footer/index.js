3c4ea0c29e852b2e4fe50f3702cbb74943dc6413
pawlo4-94 <pawlo4-94@o2.pl>
2021-09-21 17:12:50 +0200

    Info about authors added

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -4,7 +4,8 @@ export const Footer = () => (
     <PageFooter>
         <PageFooterContainer>
             <PageFooterLogo />
-            This product uses the TMDB API but is not endorsed or certified by TMDB
+            This product uses the TMDB API but is not endorsed or certified by TMDB.<br />
+            Created by Pawlo1994, Krzysztof-Jaczewski and Eugen-save
         </PageFooterContainer>
     </PageFooter>
 );
