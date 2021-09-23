06cb481d285202ec39b7929487bcb9070ca82666
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-23 20:21:33 +0200

    totalPages added, and fixed showing placeholder

--- src/features/PeopleList/peopleSlice.js
+++ src/features/PeopleList/peopleSlice.js
@@ -2,14 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const peopleSlice = createSlice({
   name: "peopleState",
-  initialState: { people: [], status: "loading" },
+  initialState: { people: [], status: "loading", totalPeoplePages: 500 },
   reducers: {
     fetchPeople: (state) => {
       state.status = "loading";
     },
     fetchPeopleSuccess: (state, { payload: Apipeople }) => {
       state.status = "success";
-      state.people = Apipeople;
+      state.people = Apipeople.results;
+      state.totalPeoplePages = Apipeople.total_pages;
     },
     fetchPeopleError: (state) => {
       state.status = "error";
@@ -21,5 +22,7 @@ export const { fetchPeople, fetchPeopleError, fetchPeopleSuccess } =
 export const selectPeopleState = (state) => state.peopleReducer;
 export const selectPeople = (state) => selectPeopleState(state).people;
 export const selectStatus = (state) => selectPeopleState(state).status;
+export const selectTotalPeoplePages = (state) =>
+  selectPeopleState(state).totalPeoplePages;
 
 export default peopleSlice.reducer;
