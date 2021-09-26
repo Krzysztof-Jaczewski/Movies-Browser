dcaac24be03f4951329a268955e9357eb7fcd1ad
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-09-26 15:28:29 +0200

    movie reducer added

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import createSagaMiddleware from "@redux-saga/core";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import moviesReducer from "./features/MoviesList/moviesSlice";
 import peopleReducer from "./features/PeopleList/peopleSlice";
+import movieReducer from "./features/MovieDetails/movieSlice"
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,6 +10,7 @@ const sagaMiddleware = createSagaMiddleware();
 const reducer = combineReducers({
   moviesReducer: moviesReducer,
   peopleReducer: peopleReducer,
+  movieReducer: movieReducer,
 });
 
 const store = configureStore({
