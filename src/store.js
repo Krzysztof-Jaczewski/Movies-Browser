f65dc3c7f559cd4b3307e282f67730972a8da7d4
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-09-28 14:45:59 +0200

    Added new saga to root saga and reduser to store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import createSagaMiddleware from "@redux-saga/core";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import moviesReducer from "./features/MoviesList/moviesSlice";
 import peopleReducer from "./features/PeopleList/peopleSlice";
+import personReducer from "./features/PersonDetails/personSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,6 +10,7 @@ const sagaMiddleware = createSagaMiddleware();
 const reducer = combineReducers({
   moviesReducer: moviesReducer,
   peopleReducer: peopleReducer,
+  personReducer: personReducer,
 });
 
 const store = configureStore({
