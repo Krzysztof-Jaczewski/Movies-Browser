deb94ec664e57bf4820eaddef2608cc0d1dd7c71
pawlo4-94 <pawlo4-94@o2.pl>
2021-10-01 12:47:23 +0200

    view conditions for movie details added

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovie, selectMovie, selectMovieCredits, selectMovieImages } from "./movieSlice";
+import { fetchMovie, selectMovie, selectMovieCredits, selectMovieImages, selectStatus } from "./movieSlice";
 import { useEffect } from "react";
 import { Backdrop } from "../../common/Backdrop";
 import { Container } from "../../common/Container";
@@ -8,6 +8,8 @@ import { MovieInfo } from "../../common/MovieInfo";
 import { Tile } from "../../common/Tile";
 import { useGenres } from "../../fetchGenres";
 import { Title } from "../../common/Tile/styled";
+import { Loading } from "../../common/Loading";
+import { Error } from "../../common/Error";
 
 export const MovieDetails = () => {
   const { id } = useParams();
@@ -15,34 +17,36 @@ export const MovieDetails = () => {
   const { cast, crew } = useSelector(selectMovieCredits);
   console.log();
   console.log(id);
+  const status = useSelector(selectStatus);
   const genres = useGenres();
   const dispatch = useDispatch();
 
   useEffect(() => dispatch(fetchMovie({ id })), [dispatch, id]);
   const nameMovieGenres = (genre_ids) =>
-  genres && genre_ids.map((tag) => genres.find(({ id }) => id === tag).name);
+    genres && genre_ids.map((tag) => genres.find(({ id }) => id === tag).name);
 
   return (
-    <>
-      <Backdrop
+    status === "success" ?
+      <>
+        <Backdrop
           title={original_title}
           poster={backdrop_path}
           rate={vote_average}
           votes={vote_count}
         />
-      <MovieInfo 
-        poster={poster_path}
-        title={original_title}
-        description={overview}
-        date={release_date}
-        rate={vote_average}
-        votes={vote_count}
-        countries={production_countries && production_countries.map(({name}) => name)}
-      />
-      {cast && (
-        <>
-        <Title>Cast</Title>
-          <Container person>
+        <MovieInfo
+          poster={poster_path}
+          title={original_title}
+          description={overview}
+          date={release_date}
+          rate={vote_average}
+          votes={vote_count}
+          countries={production_countries && production_countries.map(({ name }) => name)}
+        />
+        {cast && (
+          <>
+            <Title>Cast</Title>
+            <Container person>
               {cast.map(
                 ({
                   id,
@@ -51,8 +55,8 @@ export const MovieDetails = () => {
                   character,
                 }) => {
                   return (
-                    <Tile 
-                    person
+                    <Tile
+                      person
                       key={id}
                       poster={profile_path}
                       character={character}
@@ -61,12 +65,12 @@ export const MovieDetails = () => {
                   );
                 }
               )}
-          </Container>
-        </>
-      )}
-      {crew && (
-        <>
-          <Title>Crew</Title>
+            </Container>
+          </>
+        )}
+        {crew && (
+          <>
+            <Title>Crew</Title>
             <Container person>
               {crew.map(
                 ({
@@ -76,7 +80,7 @@ export const MovieDetails = () => {
                   id,
                 }) => {
                   return (
-                    <Tile 
+                    <Tile
                       person
                       key={id}
                       poster={profile_path}
@@ -87,8 +91,8 @@ export const MovieDetails = () => {
                 }
               )}
             </Container>
-        </>
-      )}
- </>
- );
+          </>
+        )}
+      </> : status === "loading" ? <Loading /> : <Error />
+  );
 };
