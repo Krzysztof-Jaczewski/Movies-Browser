31ef24141ac0aebe5cf8651156c036b41dd6205d
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-10-01 18:55:42 +0200

    added new componets to PersonDetails

--- src/features/PersonDetails/index.js
+++ src/features/PersonDetails/index.js
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { Container } from "../../common/Container";
 import {
   fetchPerson,
   selectPerson,
@@ -9,10 +8,12 @@ import {
   selectStatus,
 } from "./personSlice";
 import { Tile } from "../../common/Tile";
-import { Title } from "../../common/Tile/styled";
 import { useGenres } from "../../fetchGenres";
 import { Loading } from "../../common/Loading";
 import { Error } from "../../common/Error";
+import { Details } from "../../common/Details";
+import { CastCrew } from "../../common/CastCrew";
+import { nanoid } from "@reduxjs/toolkit";
 
 export const PersonDetails = () => {
   const status = useSelector(selectStatus);
@@ -20,8 +21,9 @@ export const PersonDetails = () => {
   const { profile_path, name, birthday, place_of_birth, biography } =
     useSelector(selectPerson);
   const { crew, cast } = useSelector(selectPersonCredtis);
-  console.log();
-  console.log(id);
+
+  console.log(crew);
+  console.log(cast);
   const genres = useGenres();
   const dispatch = useDispatch();
 
@@ -30,83 +32,76 @@ export const PersonDetails = () => {
     genres && genre_ids.map((tag) => genres.find(({ id }) => id === tag).name);
   return status === "success" ? (
     <>
-      <Container details>
-        <Tile
-          details
-          poster={profile_path}
-          title={name}
-          birthday={
-            birthday &&
-            `${birthday.slice(8, 10)}.${birthday.slice(5, 7)}.
+      <Details
+        details
+        poster={profile_path}
+        title={name}
+        birthday={
+          birthday &&
+          `${birthday.slice(8, 10)}.${birthday.slice(5, 7)}.
           ${birthday.slice(0, 4)}`
-          }
-          additionalTitle={place_of_birth}
-          biography={biography}
-        />
-      </Container>
+        }
+        additionalTitle={place_of_birth}
+        biography={biography}
+      />
+
       {cast && (
-        <>
-          <Container>
-            <Title>Movies - Cast ({cast.length})</Title>
-          </Container>
-          <Container>
-            {cast.map(
-              ({
-                id,
-                poster_path,
-                title,
-                release_date,
-                genre_ids,
-                vote_average,
-                vote_count,
-              }) => {
-                return (
-                  <Tile
-                    key={id}
-                    poster={poster_path}
-                    title={title}
-                    subtitle={release_date.slice(0, 4)}
-                    genres={nameMovieGenres(genre_ids)}
-                    rate={vote_average}
-                    votes={vote_count}
-                  />
-                );
-              }
-            )}
-          </Container>
-        </>
+        <CastCrew header={`Movies - Cast (${cast.length})`}>
+          {cast.map(
+            ({
+              poster_path,
+              title,
+              release_date,
+              genre_ids,
+              vote_average,
+              vote_count,
+              character,
+            }) => {
+              return (
+                <Tile
+                  key={nanoid()}
+                  poster={poster_path}
+                  title={title}
+                  subtitle={
+                    release_date && `${character} (${release_date.slice(0, 4)})`
+                  }
+                  genres={nameMovieGenres(genre_ids)}
+                  rate={vote_average}
+                  votes={vote_count}
+                />
+              );
+            }
+          )}
+        </CastCrew>
       )}
       {crew && (
-        <>
-          <Container>
-            <Title>Movies - Crew ({crew.length})</Title>
-          </Container>
-          <Container>
-            {crew.map(
-              ({
-                id,
-                poster_path,
-                title,
-                release_date,
-                genre_ids,
-                vote_average,
-                vote_count,
-              }) => {
-                return (
-                  <Tile
-                    key={id}
-                    poster={poster_path}
-                    title={title}
-                    subtitle={release_date}
-                    genres={nameMovieGenres(genre_ids)}
-                    rate={vote_average}
-                    votes={vote_count}
-                  />
-                );
-              }
-            )}
-          </Container>
-        </>
+        <CastCrew header={`Movies - Crew  (${crew.length})`}>
+          {crew.map(
+            ({
+              poster_path,
+              title,
+              job,
+              release_date,
+              genre_ids,
+              vote_average,
+              vote_count,
+            }) => {
+              return (
+                <Tile
+                  key={nanoid()}
+                  poster={poster_path}
+                  title={title}
+                  subtitle={
+                    release_date && `${job} (${release_date.slice(0, 4)})`
+                  }
+                  genres={nameMovieGenres(genre_ids)}
+                  rate={vote_average}
+                  votes={vote_count}
+                />
+              );
+            }
+          )}
+        </CastCrew>
       )}
     </>
   ) : status === "loading" ? (
