b2eaf7b7b0561dc43711e4a96d272d0bcb190fda
pawlo4-94 <pawlo4-94@o2.pl>
2021-10-03 01:43:11 +0200

    peopleResults, searchStatus and totalResultsPages deleted

--- src/features/PeopleList/peopleSlice.js
+++ src/features/PeopleList/peopleSlice.js
@@ -4,11 +4,8 @@ const peopleSlice = createSlice({
   name: "peopleState",
   initialState: {
     people: [],
-    peopleResults: [],
     status: "loading",
-    searchStatus: "loading",
     totalPeoplePages: 500,
-    totalPeopleResultsPages: 500,
   },
   reducers: {
     fetchPeople: (state) => {
@@ -22,34 +19,17 @@ const peopleSlice = createSlice({
     fetchPeopleError: (state) => {
       state.status = "error";
     },
-    fetchPeopleResults: (state) => {
-      state.searchStatus = "loading";
-    },
-    fetchPeopleResultsSuccess: (state, { payload: Apipeople }) => {
-      state.searchStatus = "success";
-      state.people = Apipeople.results;
-      state.totalPeopleResultsPages = Apipeople.total_pages;
-    },
-    fetchPeopleResultsError: (state) => {
-      state.searchStatus = "error";
-    },
   },
 });
 export const {
   fetchPeople,
   fetchPeopleError,
-  fetchPeopleSuccess,
-  fetchPeopleResults,
-  fetchPeopleResultsSuccess,
-  fetchPeopleResultsError } =
+  fetchPeopleSuccess } =
   peopleSlice.actions;
 export const selectPeopleState = (state) => state.peopleReducer;
 export const selectPeople = (state) => selectPeopleState(state).people;
 export const selectStatus = (state) => selectPeopleState(state).status;
 export const selectTotalPeoplePages = (state) =>
   selectPeopleState(state).totalPeoplePages;
-export const selectPeopleResults = (state) => selectPeopleState(state).peopleResults;
-export const selectSearchStatus = (state) => selectPeopleState(state).searchStatus;
-export const selectTotalPeopleStatusPages = (state) => selectPeopleState(state).totalPeopleResultsPages;
 
 export default peopleSlice.reducer;
