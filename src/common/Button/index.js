99b72c9bd05a5efa6931257bc1f65058d612bfe0
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-10-05 17:40:14 +0200

    Button created

---
+++ src/common/Button/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 16px 32px;
+  background-color: blue;
+  color: white;
+  margin: 24px 0;
+  border-radius: 24px;
+  transition: all 0.5s;
+
+  &:hover {
+    transform: scale(1.08);
+  }
+  &:active {
+    transform: scale(0.92);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin: 12px auto 12px 16px;
+    padding: 12px 24px;
+  }
+`;
