06248afb5c75780755b9a9f91abaf2b7ccc086f5
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-10-05 11:54:07 +0200

    Navigation fixed

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -37,6 +37,7 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-right: 16px;
 `;
 
 export const StyledLogo = styled(Logo)`
